a87a438cf1300d772fe347219dde1909a0a1a0c6
Hurley <arthursobral16@users.noreply.github.com>
2022-06-30 19:16:58 -0300

    Mudando a porta

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
 });
   
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('server is running');
 });
   
